24d1504f6e6be2596dfc5b4790135e8191384ba5
ahmed gouda <ahmed.gouda@maxab.io>
2021-10-21 15:02:35 +0200

    intiate amster

---
+++ src/main/java/com/tech/demo/Test.java
@@ -0,0 +1,10 @@
+package com.tech.demo;
+
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Test {
+    public int test (){
+        return  1;
+    }
+}
