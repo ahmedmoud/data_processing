418f08d80dbd451ec1ad0425e524401f4a07ba4a
ahmed gouda <ahmed.gouda@maxab.io>
2021-10-24 11:29:15 +0200

    push

--- src/main/java/com/tech/demo/repositories/UserRepository.java
+++
@@ -1,6 +0,0 @@
-package com.tech.demo.repositories;
-import com.tech.demo.models.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface  UserRepository extends JpaRepository<User, Integer>{
-}
